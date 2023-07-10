ea28ada3ce5203cbd6fa2fa4c1e52ff7790a1a68
Hava Isr <hava.isrplv@gmail.com>
2023-07-10 07:52:19 +0300

    Initial Commit

--- JavaOdevi02/src/Main.java
+++ JavaOdevi02/src/Main.java
@@ -1,21 +1,162 @@
+import model.Category;
+import model.Film;
+import model.Platform;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        System.out.println("Welcome! Please enter 'A' if you are an Admin, please enter 'C' if you are a Client.");
-        Scanner signIn = new Scanner(System.in);
-        String user = signIn.next();
 
-        if (user.equals("A")){
-            System.out.println("You are an Admin!");
-        } else if (user.equals("C")) {
-            System.out.println("You are a Client!");
-        } else {
-            System.out.println("Wrong input please re-enter.");
-        }
+        ArrayList<Film> FilmList = new ArrayList<>();
+        ArrayList<Category> categoryList = new ArrayList<>();
+        ArrayList<Platform> platformList = new ArrayList<>();
+        List<String> screeningsList = new ArrayList<>();
+        List<Category> filmCategories = new ArrayList<>();
+        List<Platform> filmPlatforms = new ArrayList<>();
 
+        String user;
+        int entry;
+        boolean ask = true;
+
+
+        do {
+            System.out.println("Welcome! Please enter 'A' if you are an Admin, please enter 'C' if you are a Client. Enter 'X' for exit.");
+            Scanner input = new Scanner(System.in);
+            user = input.next();
+
+            switch (user) {
+                case "A":
+                    System.out.println("Welcome Admin!");
+                    System.out.println("Please choose to Enter: 1-Category 2-Platform 3-Film");
+                    entry = input.nextInt();
+                    input.nextLine();
+
+                    switch (entry) {
+                        case 1:
+
+                            System.out.println("Please enter the following information.");
+
+                            System.out.println("Name of the Category:");
+                            String newCategoryName = input.nextLine();
+                            Category newCategory = new Category();
+                            newCategory.setCategoryName(newCategoryName);
+                            categoryList.add(newCategory);
+
+                            break;
+
+                        case 2:
+                            System.out.println("Name of the Platform:");
+                            String newPlatformName = input.nextLine();
+                            Platform newPlatform = new Platform();
+                            newPlatform.setPlatformName(newPlatformName);
+                            platformList.add(newPlatform);
+
+                            break;
+
+                        case 3:
+
+                            System.out.println("Name of the film:");
+                            String name = input.nextLine();
+
+                            System.out.println("Year of the film:");
+                            int year = input.nextInt();
+                            input.nextLine();
+
+                            System.out.println("Director of the film:");
+                            String director = input.nextLine();
+
+                            System.out.println("Imdb rating of the film:");
+                            double imdb = input.nextDouble();
+                            input.nextLine();
+
+                            System.out.print("Enter the number of screeningsList for this film: ");
+                            int n;
+                            n = input.nextInt();
+                            input.nextLine();
+
+                            System.out.println("Screening time of the film:");
+                            for (int i = 0; i < n; i++) {
+                                System.out.println("Screening-" + (i + 1) + ": ");
+                                String screening = input.next();
+                                screeningsList.add(screening);
+                            }
+
+                            System.out.print("Enter the number of categories for this film: ");
+                            int c;
+                            c = input.nextInt();
+                            input.nextLine();
+
+                            System.out.println("Choose categories for the film.");
+                            for (int i = 0; i < categoryList.size(); i++) {
+                                System.out.println((i + 1) + "-" + categoryList.get(i).getCategoryName());
+                            }
+                            for (int i = 0; i < c; i++) {
+                                System.out.println("Category-" + (i + 1) + ": ");
+                                int catNum = input.nextInt();
+                                input.nextLine();
+                                filmCategories.add(categoryList.get(catNum - 1));
+
+                            }
+
+
+                            System.out.print("Enter the number of platforms for this film: ");
+                            int p;
+                            p = input.nextInt();
+                            input.nextLine();
+
+                            System.out.println("Choose platforms for the film.");
+                            for (int i = 0; i < platformList.size(); i++) {
+                                System.out.println((i + 1) + "-" + platformList.get(i).getPlatformName());
+                            }
+                            for (int i = 0; i < p; i++) {
+                                System.out.println("Platform-" + (i + 1) + ": ");
+                                int platNum = input.nextInt();
+                                input.nextLine();
+                                filmPlatforms.add(platformList.get(platNum - 1));
+
+                            }
+
+                            Film newFilm = new Film(name, year, director, imdb, screeningsList, categoryList, platformList);
+                            FilmList.add(newFilm);
+
+
+                    }
+
+                    break;
+
+                case "C":
+                    System.out.println("Welcome!");
+                    System.out.println("Please enter the category you want to see.");
+                    for (int i = 0; i < categoryList.size(); i++) {
+                        System.out.println((i + 1) + "-" + categoryList.get(i).getCategoryName());
+                    }
+                    int userCat = input.nextInt();
+                    input.nextLine();
+
+                    System.out.println("Films in category '" + categoryList.get(userCat - 1).getCategoryName() + "':");
+                    for (Film film : FilmList) {
+                        for (Category category : film.getCategoryArrayList()) {
+                            if (category.getCategoryName().equals(categoryList.get(userCat - 1).getCategoryName())) {
+                                System.out.println(film);
+                            }
+                        }
+                    }
+
+                    break;
+                case "X":
+                    System.out.println("You are exiting.");
+                    ask = false;
+                    break;
+
+                default:
+                    System.out.println("Wrong input please re-enter.");
+                    break;
+            }
+
+        } while (ask);
 
     }
 }
---
+++ JavaOdevi02/src/model/Film.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Film {
+
+    private String name;
+    private int year;
+    private String director;
+    private double imdb;
+    private ArrayList<Category> categoryArrayList = new ArrayList<>();
+    private List<String> screenTime;
+    private ArrayList<Platform> platformArrayList = new ArrayList<>();
+
+
+    public Film(String name, int year, String director, double imdb, List<String> screenTime, ArrayList<Category> categoryArrayList, ArrayList<Platform> platformArrayList) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.imdb = imdb;
+        this.screenTime = screenTime;
+        this.categoryArrayList = categoryArrayList;
+        this.platformArrayList = platformArrayList;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public double getImdb() {
+        return imdb;
+    }
+
+    public void setImdb(double imdb) {
+        this.imdb = imdb;
+    }
+
+
+    public List<String> getScreenTime() {
+        return screenTime;
+    }
+
+    public void setScreenTime(List<String> screenTime) {
+        this.screenTime = screenTime;
+    }
+
+    public ArrayList<Category> getCategoryArrayList() {
+        return categoryArrayList;
+    }
+
+    public void setCategoryArrayList(ArrayList<Category> categoryArrayList) {
+        this.categoryArrayList = categoryArrayList;
+    }
+
+    public ArrayList<Platform> getPlatformArrayList() {
+        return platformArrayList;
+    }
+
+    public void setPlatformArrayList(ArrayList<Platform> platformArrayList) {
+        this.platformArrayList = platformArrayList;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "Name= " + name + "\n" +
+                "Year = " + year + "\n" +
+                "Director = '" + director + "\n" +
+                "Imdb Rating= " + imdb + "\n" +
+                "Category = " + categoryArrayList + "\n" +
+                "Screening Time = '" + screenTime + "\n" +
+                "Platform = " + platformArrayList +
+                '}';
+    }
+}
