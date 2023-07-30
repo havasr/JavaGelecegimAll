0fbd286089a7adee6567766e4b193c57a8c6166f
Hava Isr <hava.isrplv@gmail.com>
2023-07-30 16:34:02 +0300

    GenerateTranscript updated

--- TranscriptManager/src/hw3/main/GenerateTranscript.java
+++ TranscriptManager/src/hw3/main/GenerateTranscript.java
@@ -2,73 +2,126 @@ package hw3.main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GenerateTranscript {
 
-    public Transcript takeInputFromUser() {
+//Prompt user to choose a way to generate transcript, call the respective method
+    public Transcript generateTranscript(){
         Scanner input = new Scanner(System.in);
+        System.out.println("Choose an option to generate transcript:");
+        System.out.println("1. Input data manually.");
+        System.out.println("2. Take data from file.\n");
 
-        System.out.println("Please enter information for all classes. \nDisclaimer: In case of invalid input, default values will be set.");
+        String option = input.nextLine();
+        Transcript transcript = null;
 
+        if (option.equals("1")) {
+            transcript = takeInputFromUser(input);
+        } else if (option.equals("2")) {
+            transcript = takeInputFromFile(input);
+        } else {
+            System.out.println("Invalid option.");
+        }
+        return transcript;
+    }
+
+    // Method to take input from the user to create a transcript
+    public Transcript takeInputFromUser(Scanner input) {
+        System.out.println("Please enter data for all classes. " +
+                "\nDisclaimer: In case the respective data does not exist please enter '0'. Student ID is mandatory.");
 
         System.out.println("Please enter the student ID: ");
         Transcript transcript = null;
         while (input.hasNext()) {
-            int studentID = Integer.parseInt(input.nextLine());
 
+            int studentID = 0;
+            try {
+                studentID = input.nextInt();
+                input.nextLine();
+            } catch (InputMismatchException e){
+                System.out.println("Invalid input for student ID.");
+                break;
+            }
+
+            // Create a new Transcript object for the student
             transcript = new Transcript(studentID);
 
+            // Loop to take input for each course taken by the student
             while (true) {
                 System.out.print("Enter Department: ");
                 String department = input.nextLine();
+                // Check if the user wants to end the input for courses
                 if (department.equalsIgnoreCase("end")) {
                     break;
                 }
 
-                System.out.print("Enter Course Code: ");
-                int courseCode = input.nextInt();
+                int courseCode = 0;
+                int credit = 0;
+                double grade = 0.0;
 
-                System.out.print("Enter Credit: ");
-                int credit = Integer.parseInt(input.nextLine());
+                try {
+                    System.out.print("Enter Course Code: ");
+                    courseCode = input.nextInt();
 
-                System.out.print("Enter Grade: ");
-                double grade = Double.parseDouble(input.nextLine());
-                System.out.println("If you want to end course input, please enter 'end'.\n If not, please continue.");
+                    System.out.print("Enter Credit: ");
+                    credit = input.nextInt();
 
+                    System.out.print("Enter Grade: ");
+                    grade = input.nextDouble();
+                    input.nextLine();
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input for course details. Please re-enter details.");
+                    input.nextLine();
+                    continue; // Go to the next iteration to re-ask for the course details
+                }
 
+                System.out.println("If you want to end course input, please enter 'end'." +
+                        "\nIf not, please continue.");
+
+                // Create a new CourseGrade object for the course and add it to the transcript
                 CourseGrade courseGrade = new CourseGrade(department, courseCode, credit, grade);
                 transcript.addCourseTaken(courseGrade);
             }
-            System.out.println("\nTo terminate input, use the EOF indicator. For Windows Ctrl + Z, for Linux, macOS Ctrl + D");
+            // Prompt for termination of input using the appropriate EOF indicator
+            System.out.println
+                    ("\nTo terminate input, use the EOF indicator. For Windows Ctrl + Z, for Linux, macOS Ctrl + D");
+
         }
         System.out.println(transcript);
         return transcript;
-
     }
 
-    public Transcript takeInputFromFile() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please enter the file name:  ");
-        String pathName = "src/hw3/assets/" + input.nextLine();
+    // Method to take input from a file to create a transcript
+    public Transcript takeInputFromFile(Scanner input) {
+        // Prompt for the file name from which to read the input
+        System.out.print("Enter filename:  ");
+        //File named Transcript.txt has been used to check code
+        String fileName = input.nextLine();
+        String pathName = "src/hw3/assets/" + fileName;
 
         File file = new File(pathName);
         Transcript transcript = null;
         try {
             Scanner scanner = new Scanner(file);
 
-            int studentId = Integer.parseInt(scanner.nextLine());
+            // Read the student ID from the first line of the file
+            int studentId = scanner.nextInt();
+            scanner.nextLine();
 
             transcript = new Transcript(studentId);
 
+            // Loop to read input for each course taken by the student from the file
             while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] parts = line.split(" ");
+                String lines = scanner.nextLine();
+                String[] parts = lines.split(" ");
                 String department = parts[0];
                 int courseCode = Integer.parseInt(parts[1]);
                 int credit = Integer.parseInt(parts[2]);
                 double grade = Double.parseDouble(parts[3]);
 
+                // Create a new CourseGrade object for the course and add it to the transcript
                 CourseGrade courseGrade = new CourseGrade(department, courseCode, credit, grade);
                 transcript.addCourseTaken(courseGrade);
             }
@@ -76,6 +129,7 @@ public class GenerateTranscript {
             System.out.println(transcript);
             scanner.close();
         } catch (FileNotFoundException e) {
+            // Handle the case when the specified file is not found
             System.out.println("File not found: " + pathName);
         }
         return transcript;
