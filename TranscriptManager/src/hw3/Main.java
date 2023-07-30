9177e5dd16a22506c0bee977815512c42056209a
Hava Isr <hava.isrplv@gmail.com>
2023-07-30 14:05:40 +0300

    Main modified.

--- TranscriptManager/src/hw3/Main.java
+++ TranscriptManager/src/hw3/Main.java
@@ -1,11 +1,14 @@
 package hw3;
+
 import hw3.main.CourseGrade;
+import hw3.main.GenerateTranscript;
 import hw3.main.Transcript;
 import hw3.util.Grade;
 
 public class Main {
     public static void main(String[] args) {
 
+        /*
         CourseGrade courseGrade1 = new CourseGrade("ME", 120, 4, 3.5);
         CourseGrade courseGrade2 = new CourseGrade("ece", 20, 3, Grade.D);
         CourseGrade courseGrade3 = new CourseGrade("ABC", 60, 1, 6);
@@ -18,6 +21,10 @@ public class Main {
         transcript1.addCourseTaken(courseGrade3);
 
         System.out.println(transcript1);
+*/
+        GenerateTranscript generateTranscript = new GenerateTranscript();
+        Transcript transcript2 = generateTranscript.takeInputFromUser();
+        Transcript transcript3 =generateTranscript.takeInputFromFile();
 
     }
 }
